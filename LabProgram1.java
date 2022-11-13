6862491a128c809b2d48efa55e93d038e85a8167
Nishithareddytr <113182847+Nishithareddytr@users.noreply.github.com>
2022-11-13 15:57:10 +0530

    Update LabProgram1.java

--- LabProgram1.java
+++ LabProgram1.java
@@ -1,17 +1,28 @@
 // Invoice Check Program
+Create a class called invoice that a hardware store might use to represent an invoice for an item sold at the store.An invoice should include four pieces of 
+information as instance variables, a part number(type string), a part description (type string), a quantity of the item being purchased (type int) and
+a price per item(double).Your class should have a constructor that initializes the four instance variables. Provide a set and a get method for each instance variable. 
+In addition, provide a method named getInvoiceAmount that calculates the invoice amount (i.e., multiplies the quantity by the price per item), then returns the amount 
+as a double value .If the quantity is not positive, it should be set to 0.If the price per item is not positive ,it should be set to 0.0.
+
+Write a test application named InvoiceTest that demonstrate class Invoice’s capabilities.
+
 
 // Main Class which prints invoice data
-public class InvoiceCheck {
+public class InvoiceTest {
     public static void main(String[] args) {
-        // Creating an invoice Obj
+        // Creating an invoice Obj by calling parameterized constructor
         Invoice invoiceObj = new Invoice("62", "Fruit", 12, 2.5);
+        // Printing the values of first item
         System.out.println("Part number: " + invoiceObj.getPartNumber() + "; Part description: " + invoiceObj.getPartDescription() +
                 "; Sold: " + invoiceObj.getQuantityPurchased() + "; Price: " + invoiceObj.getItemPrice() + "; Invoice amount: " +
                 invoiceObj.getInvoiceAmount());
+        // Setting the new values
         invoiceObj.setPartNumber("65");
         invoiceObj.setPartDescription("Car");
         invoiceObj.setQuantityPurchased(2);
         invoiceObj.setItemPrice(150000.00);
+        // Printing the new values
         System.out.println("After Updating the values:");
         System.out.println("Part number: " + invoiceObj.getPartNumber() + "; Part description: " + invoiceObj.getPartDescription() +
                 "; Sold: " + invoiceObj.getQuantityPurchased() + "; Price: " + invoiceObj.getItemPrice() + "; Invoice amount: " +
