9ed5197075b104315327808e90dfeba7f90636fb
Nishithareddytr <113182847+Nishithareddytr@users.noreply.github.com>
2022-11-15 12:52:06 +0530

    Update LabProgram3.java

--- LabProgram3.java
+++ LabProgram3.java
@@ -1,30 +1,13 @@
-Create class SavingsAccount. Use a static variable annualInterestRate to store the annual
-
-interest rate for all account holders. Each object of the class contains a private instance variable
-
-savingsBalance indicating the amount the saver currently has on deposit. Provide method
-
-calculateMonthlyInterest to calculate the monthly interest by multiplying the
-
-savingsBalance by annualInterestRate divided by 12—this interest should be added to
-
-savingsBalance. Provide a method modifyInterestRate that sets the
-
-annualInterestRate to a new value.
-
-Write a program to test class SavingsAccount. Instantiate two savingsAccount objects, saver1
-
-and saver2, with balances of $2000.00 and $3000.00, respectively. Set annualInterestRate to
-
-4%, then calculate the monthly interest for each of 12 months and print the new balances for both savers.
-
-Next, set the annualInterestRate to 5%, calculate the next month’s interest and print the new
-
-balances for both savers. Also, provide the UML class diagram of your class
+Create class SavingsAccount. Use a static variable annualInterestRate to store the annualinterest rate for all account holders. Each object of the class contains a 
+private instance variablesavingsBalance indicating the amount the saver currently has on deposit. Provide methodcalculateMonthlyInterest to calculate the monthly
+interest by multiplying thesavingsBalance by annualInterestRate divided by 12—this interest should be added tosavingsBalance. Provide a method modifyInterestRate 
+that sets tannualInterestRate to a new value.Write a program to test class SavingsAccount. Instantiate  two savingsAccount objects,saver1and saver2, with balances of $2000.00 and $3000.00, respectively. 
+Set annualInterestRate to4%, then calculate the monthly interest for each of 12 months and print the new balances for both savers.Next, set the annualInterestRate 
+to 5%, calculate the next month’s interest and print the new balances for both savers. Also, provide the UML class diagram of your class
 
 PROGRAM
 
-public class SavingsAccount {
+class SavingsAccount {
     private static double annualInterestRate = 0;
     private double savingsBalance;
 
@@ -46,7 +29,7 @@ public class SavingsAccount {
 }
 
 
-public class Main {
+public class  SavingsAccount {
     public static void main(String[] args) {
         SavingsAccount saver1 = new SavingsAccount(2000);
         SavingsAccount saver2 = new SavingsAccount(3000);
